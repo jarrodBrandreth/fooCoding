962b65f1f59387b9765fc9e5bf20060c6384d948
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2022-05-31 12:56:46 +0200

    created about tab and changed js to work with it

--- Javascript2/week2/app.js
+++ Javascript2/week2/app.js
@@ -7,6 +7,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2003,
     genre: 'Fiction',
+    description: `NATIONAL BESTSELLER The first volume in the internationally acclaimed MaddAddam trilogy is at once
+     an unforgettable love story and a compelling vision of the future-from the bestselling author of The Handmaid's
+      Tale and The Testaments.`,
+    about: 'about',
   },
   year_of_the_flood: {
     title: 'Year of the Flood',
@@ -14,6 +18,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2009,
     genre: 'Fiction',
+    description: `NATIONAL BESTSELLER From the bestselling author of The Handmaid's Tale and The Testaments-the second
+     book of the internationally celebrated MaddAddam trilogy, set in the visionary world of Oryx and Crake, is at once
+      a moving tale of lasting friendship and a landmark work of speculative fiction.`,
+    about: 'about',
   },
   maddaddam: {
     title: 'MaddAddam',
@@ -21,6 +29,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2013,
     genre: 'Fiction',
+    description: `NATIONAL BESTSELLER From the bestselling author of The Handmaid's Tale and The Testamants-this final volume
+     of the internationally celebrated MaddAddam trilogy "has brought the previous two books together in a fitting and joyous
+      conclusion that's an epic not only of an imagined future but of our own past" (The New York Times Book Review).`,
+    about: 'about',
   },
   sapiens: {
     title: 'Sapiens: A Brief History of Humankind',
@@ -28,6 +40,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2014,
     genre: 'Non-Fiction',
+    description: `NEW YORK TIMES BESTSELLER From a renowned historian comes a groundbreaking narrative of humanity's creation
+     and evolution a #1 international bestseller that explores the ways in which biology and history have defined us and enhanced
+      our understanding of what it means to be "human."`,
+    about: 'about',
   },
   homo_deus: {
     title: 'Homo Deus: A Brief History of Tomorrow',
@@ -35,6 +51,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2016,
     genre: 'Non-Fiction',
+    description: `NEW YORK TIMES BESTSELLER Yuval Noah Harari, author of the critically-acclaimed New York Times bestseller and
+     international phenomenon Sapiens, returns with an equally original, compelling, and provocative book, turning his focus toward
+      humanity's future, and our quest to upgrade humans into gods.`,
+    about: 'about',
   },
   amusing_ourselves_to_death: {
     title: 'Amusing Ourselves to Death: Public Discourse in the Age of Show Business',
@@ -42,6 +62,12 @@ const bookTitles = {
     language: 'English',
     year_published: 1985,
     genre: 'Non-Fiction',
+    description: `Television has conditioned us to tolerate visually entertaining material measured out in spoonfuls of time, to the
+     detriment of rational public discourse and reasoned public affairs. In this eloquent, persuasive book, Neil Postman alerts us to
+      the real and present dangers of this state of affairs, and offers compelling suggestions as to how to withstand the media onslaught.
+       Before we hand over politics, education, religion, and journalism to the show business demands of the television age, we must recognize
+        the ways in which the media shape our lives and the ways we can, in turn, shape them to serve out highest goals.`,
+    about: 'about',
   },
   conscientious_objections: {
     title: 'Conscientious Objections: Stirring Up Trouble About Language, Technology and Education',
@@ -49,6 +75,14 @@ const bookTitles = {
     language: 'English',
     year_published: 1988,
     genre: 'Non-Fiction',
+    description: `In a series of feisty and ultimately hopeful essays, one of America's sharpest social critics casts a shrewd eye over
+     contemporary culture to reveal the worst - and the best - of our habits of discourse, tendencies in education, and obsessions with
+      technological novelty. Readers will find themselves rethinking many of their bedrock assumptions: Should education transmit culture
+       or defend us against it? Is technological innovation progress or a peculiarly American addiction? When everyone watches the same
+        television programs - and television producers don't discriminate between the audiences for Sesame Street and Dynasty - is childhood
+         anything more than a sentimental concept? Writing in the traditions of Orwell and H.L. Mencken, Neil Postman sends shock waves of wit
+          and critical intelligence through the cultural wasteland.`,
+    about: 'about',
   },
   brave_new_world: {
     title: 'Brave New World',
@@ -56,6 +90,11 @@ const bookTitles = {
     language: 'English',
     year_published: 1932,
     genre: 'Fiction',
+    description: `Brave New World is a dystopian social science fiction novel by English author Aldous Huxley, written in 1931 and published in 1932.
+     Largely set in a futuristic World State, whose citizens are environmentally engineered into an intelligence-based social hierarchy, the novel
+      anticipates huge scientific advancements in reproductive technology, sleep-learning, psychological manipulation and classical conditioning that
+       are combined to make a dystopian society which is challenged by only a single individual: the story's protagonist.`,
+    about: 'about',
   },
   born_a_crime: {
     title: 'Born a Crime: Stories From a South African Childhood',
@@ -63,6 +102,10 @@ const bookTitles = {
     language: 'English',
     year_published: 2016,
     genre: 'Non-Fiction',
+    description: `Born a Crime is the story of a mischievous young boy who grows into a restless young man as he struggles to find himself in a
+     world where he was never supposed to exist. It is also the story of that young man's relationship with his fearless, rebellious, and fervently
+      religious mother-his teammate, a woman determined to save her son from the cycle of poverty, violence, and abuse that would ultimately threaten her own life.`,
+    about: 'about',
   },
   animal_farm: {
     title: 'Animal Farm',
@@ -70,6 +113,12 @@ const bookTitles = {
     language: 'English',
     year_published: 1945,
     genre: 'Fiction',
+    description: `All the animals were looking forward to the new, happy lives they would lead when Mr Jones's rule over them came to an end.
+     When the animals get rid of Mr. Jones and take over the farm, they are excited by their new freedom. But can they run the farm with tools
+      that are designed for humans? Will they be able to harvest the food that they need for the winter? The farmers decide to band together to
+       win back the farm for Mr. Jones. Will the animals be able to fight them off? Can life on Animal Farm be as wonderful as they had all hoped
+        for, or will new dangers threaten their happiness?`,
+    about: 'about',
   },
 };
 
@@ -184,7 +233,8 @@ const showBookInfo = (e) => {
   const para = e.composedPath()[1].children;
   //console.log(e.composedPath()[1].children);
   //console.log(e.path[1].children);
-  for (let i = 1; i < para.length - 2; i++) {
+  para[5].classList.add('slide-in');
+  for (let i = 1; i < para.length - 4; i++) {
     para[i].classList.add('appear');
   }
   e.target.removeEventListener('click', showBookInfo);
@@ -193,7 +243,8 @@ const showBookInfo = (e) => {
 
 const removeBookInfo = (e) => {
   const para = e.composedPath()[1].children;
-  for (let i = 1; i < para.length - 2; i++) {
+  para[5].classList.remove('slide-in');
+  for (let i = 1; i < para.length - 4; i++) {
     para[i].classList.remove('appear');
   }
   e.target.removeEventListener('click', removeBookInfo);
@@ -207,8 +258,7 @@ const pageContainer = document.querySelector('.page-container');
 pageContainer.appendChild(createBooksList(bookTitles));
 addCoverImg(bookImgSrc);
 addLinks(goodReadsLink);
-const bookCoverContainer = document.querySelectorAll('.book-cover-container');
-
-bookCoverContainer.forEach((cover) => {
-  cover.addEventListener('click', showBookInfo);
+const aboutTab = document.querySelectorAll('.about');
+aboutTab.forEach((tab) => {
+  tab.addEventListener('click', showBookInfo);
 });
